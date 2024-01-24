b3220a6fc66a2088dbb9e7b42c147d1c0174cd62
Sach Rao <rao.sachith@gmail.com>
2024-01-23 20:25:37 -0600

    initial commit for todo list

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+// need 3 diff event listener clicks. 
+// user interactions- 
+/*
+1. delete item with trash
+2. mark complete
+3. mark uncomplete
+
+*/
+
+const deleteBtn = document.querySelectorAll('.fa-trash')
+const item = document.querySelectorAll('.item span')
+const itemCompleted = document.querySelectorAll('.item span.completed')
+
+Array.from(deleteBtn).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
+})
+
+Array.from(item).forEach((element)=>{
+    element.addEventListener('click', markComplete)
+})
+
+Array.from(itemCompleted).forEach((element)=>{
+    element.addEventListener('click', markUnComplete)
+})
+
+async function deleteItem(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markUnComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markUnComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
---
+++ server.js
@@ -0,0 +1,84 @@
+// require modules
+
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+// set db string connection 
+
+let db,
+    dbConnectionStr = process.env.DB_CONNECTION,
+    dbName = 'todo'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+    
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+
+app.get('/', async (request, response) => {
+    const todoItems = await db.collection('todos').find().toArray()
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+})
+
+
+
+app.post('/addTodo', async (request, response) => {
+    await db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    console.log('Todo Added')
+    response.redirect('/')
+})
+
+
+app.delete('/deleteItem', async (request, response) => {
+    await db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    console.log('Todo deleted')
+    response.json('Todo deleted')
+
+})
+
+app.put('/markComplete', async (request, response) => {
+    await db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        $set: {
+            completed: true
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+
+    console.log('Marked Complete')
+    response.json('Marked Complete')
+
+})
+
+
+app.put('/markUnComplete', async (request, response) => {
+    await db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        $set: {
+            completed: false
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+
+    console.log('Marked Completed')
+    response.json('Marked Completed')
+
+
+})
